acdc65f045eef3abcab2f9259a83f44db6731bd3
r4v3n6101 <raven6107@gmail.com>
2026-03-01 03:06:23 +0300

    nix: replace rust-overlay with naersk, decreasing size of dependencies

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -12,27 +15,30 @@
       self,
       nixpkgs,
       flake-utils,
-      rust-overlay,
+      naersk,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ rust-overlay.overlays.default ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rustVersion = pkgs.rust-bin.nightly.latest.default;
+        pkgs = import nixpkgs { inherit system; };
+        naersk' = pkgs.callPackage naersk { };
       in
       {
         formatter = pkgs.nixpkgs-fmt;
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+        };
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            (rustVersion.override {
-              extensions = [
-                "rust-src"
-                "rust-analyzer"
-              ];
-            })
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            rust-analyzer
+            rustPackages.clippy
           ];
+
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
         };
       }
     );
